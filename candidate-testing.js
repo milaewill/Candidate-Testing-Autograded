fe64b02a72bd1f97ff7ae4f8a933bb229e14a575
Mila Williams <milaewill@Milas-MBP.lan>
2023-09-25 15:25:33 -0500

    finished

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,14 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "];
+
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -23,7 +28,10 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question)
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswer = input.question(questions[i])
+    candidateAnswers.push(candidateAnswer)
+  }
 
 }
 
@@ -36,11 +44,22 @@ function gradeQuiz(candidateAnswers) {
     console.log("Incorrect!")
   }
 
+  console.log(`Candidate Answers: ${candidateAnswers} Correct Answers: ${correctAnswers}`)
+
+  let score = 0;
+
+  for (let i = 0; i < correctAnswers.length; i++) {
+    if (candidateAnswers[i].toUpperCase() === correctAnswers[i].toUpperCase())
+      score += 1
+  }
 
 
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
+  let grade = score / candidateAnswers.length * 100;  //TODO 3.2 use this variable to calculate the candidates score.
+  if (grade < 80) {
+    console.log("Fail.")
+  } else if (grade >= 80) {
+    console.log("Pass!")
+  }
 
   return grade;
 }
